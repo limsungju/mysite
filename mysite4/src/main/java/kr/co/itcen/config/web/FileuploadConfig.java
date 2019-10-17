7619d7978306cc26d8610cec6dc2ff4b75922bee
limsungju <fullmoon4860@gmail.com>
2019-10-17 11:30:46 +0900

    Java Config : Fileupload 추가

---
+++ mysite4/src/main/java/kr/co/itcen/config/web/FileuploadConfig.java
@@ -0,0 +1,45 @@
+package kr.co.itcen.config.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 1. MultipartResolver 설정
+ *
+ */
+@Configuration
+@EnableWebMvc
+@PropertySource("classpath:kr/co/itcen/mysite/config/web/properties/multipart.properties")
+public class FileuploadConfig extends WebMvcConfigurerAdapter {
+	
+	@Autowired
+	private Environment env;
+	
+	// MultipartResolver
+	@Bean
+	public CommonsMultipartResolver commonsMultipartResolver() {
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setMaxUploadSize(env.getProperty("multipart.maxUploadSize", Integer.class));
+		multipartResolver.setMaxInMemorySize(env.getProperty("multipart.maxInMemorySize", Integer.class));
+		multipartResolver.setDefaultEncoding(env.getProperty("multipart.defaultEncoding"));
+		
+		return multipartResolver;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+			.addResourceHandler(env.getProperty("multipart.resourceMapping") + "/**")
+			.addResourceLocations("file:d:" + env.getProperty("multipart.uploadsLocation"));
+	}
+	
+	
+	
+}
--- mysite4/src/main/java/kr/co/itcen/mysite/config/WebConfig.java
+++ mysite4/src/main/java/kr/co/itcen/mysite/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
+import kr.co.itcen.config.web.FileuploadConfig;
 import kr.co.itcen.config.web.MVCConfig;
 import kr.co.itcen.config.web.MessageConfig;
 import kr.co.itcen.config.web.SecurityConfig;
@@ -12,7 +13,7 @@ import kr.co.itcen.config.web.SecurityConfig;
 @Configuration
 @EnableAspectJAutoProxy // <aop:aspectj-autoproxy />
 @ComponentScan({"kr.co.itcen.mysite.controller"}) // auto-scan
-@Import({MVCConfig.class, SecurityConfig.class, MessageConfig.class})
+@Import({MVCConfig.class, SecurityConfig.class, MessageConfig.class, FileuploadConfig.class})
 public class WebConfig {
 	
 }
